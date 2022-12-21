6073550f3d48eac492943d84ff0d234f5d8b92da
Fatih <65772422+tr2cky@users.noreply.github.com>
2022-12-21 21:32:12 +0300

    Delete .history/my-app/src directory

--- .history/my-app/src/index_20221216140223.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-class H extends React.Component {
-  render() {
-    return <h1>Hello, World!</h1>;
-  }
-}
--- .history/my-app/src/index_20221216140442.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const root
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello, World!</h1>;
-  }
-}
