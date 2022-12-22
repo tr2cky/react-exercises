da681231bf443c724fcbcd8e63f69449da419e03
Fatih <kilic8118@gmail.com>
2022-12-22 16:11:44 +0300

    Props - 05

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 
 const root = document.getElementById('root');
 
+class Age extends React.Component {
+  render() {
+    return <p> Your age is {this.props.age || 0}</p>
+  }
+}
+
 class Welcome extends React.Component {
   render() {
     return <div>
       <p>
         Welcome, {this.props.name || "Undefined User"}!
       </p>
-      <p> Your age is {this.props.age || 0}</p>
+      <Age age={this.props.age} />
     </div>
   }
 }
