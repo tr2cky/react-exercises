44092b8a895e3adab57d21e56a20766b692f7250
Fatih <kilic8118@gmail.com>
2023-01-14 10:26:44 +0300

    Forms 01

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ClickCounter, Counter } from './counter';
+import { InteractiveWelcome, Welcome } from './interactive';
 
 const root = document.getElementById('root');
 
-class Age extends React.Component {
-  render() {
-    return ( <div> {this.props.age > 18 
-    ? <p>Your age is {this.props.age}</p>
-    : <p>You are very young!</p>}
-    
-    </div>)
-  }
-}
-
-class Welcome extends React.Component {
-  render() {
-    return <div>
-      <p>
-        Welcome, {this.props.name || "Undefined User"}!
-      </p>
-      {this.props.age < 65 && this.props.name == "John" && <Age age={this.props.age} />}
-    </div>
-  }
-}
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Welcome name="John" age={2} />
-        <ClickCounter />
+        <InteractiveWelcome />
       </div>
     );
   }
