e0d7c6a67ef2d5cc02c8098dd8a13557dcc96afc
Fatih <kilic8118@gmail.com>
2023-01-14 13:06:13 +0300

    Forms 03

--- my-app/src/login.js
+++ my-app/src/login.js
@@ -6,7 +6,8 @@ export class Login extends React.Component {
         name: "",
         username: "",
         password: "",
-        remember: false
+        remember: false,
+        loggedIn: false
 }
     eventHandler(event) {
         const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
@@ -16,13 +17,34 @@ export class Login extends React.Component {
             }
         })
     }
+
+
+    clickHandler() {
+        this.setState( (state) => {
+            return {
+                loggedIn: true
+            }
+        })
+    }
+
+    componentDidUpdate() {
+        console.log(this.state)
+    }
+
     render () {
         return (
             <div>
                 <input name="username" type="text" value={this.state.username} onChange={this.eventHandler.bind(this)} placeholder="Type Something"/>
                 <input name="password" type="password" value={this.state.password} onChange={this.eventHandler.bind(this)} placeholder="Type Something"/>
                 <input name="remember" type="checkbox" value={this.state.remember} onChange={this.eventHandler.bind(this)} placeholder="Type Something"/>
+                 {this.state.username && this.state.password ? 
+                 <button  onClick={this.clickHandler.bind(this)}>Login</button>
+                 :<button disabled onClick={this.clickHandler.bind(this)}>Login</button>} 
             </div>
         )
     }
 }
+
+  
+  
+  
